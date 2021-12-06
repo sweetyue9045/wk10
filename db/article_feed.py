be250aa4115037accbbd77d19de0e4b9919a6ccb
wk9 <sweetyue9045@gmail.com>
2021-12-06 10:25:37 +0800

    try update

--- db/article_feed.py
+++ db/article_feed.py
@@ -1,25 +1,25 @@
 article = [
     {
         "title": "Article1",
-        "author":"Joe",
+        "author": "Joe",
         "content":  "Article1",
         "owner_id": 1,
     },
     {
         "title": "Article2",
-        "author":"Alex",
+        "author": "Alex",
         "content":  "Article2",
         "owner_id": 2,
     },
     {
         "title": "Article3",
-        "author":"Amanda",
+        "author": "Amanda",
         "content":  "Article3",
         "owner_id": 3,
     },
     {
         "title": "Article4",
-        "author":"Joe",
+        "author": "Joe",
         "content":  "Article4",
         "owner_id": 1,
     }
